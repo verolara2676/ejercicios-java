fd688b25ccb50e7ff86284a0f831ddaa9148a29b
vero <vero@localhost.localdomain>
2017-04-06 13:09:25 -0300

    subo el dos

--- ejercicios/src/ejercicios/ejercicio2.java
+++ ejercicios/src/ejercicios/ejercicio2.java
@@ -9,5 +9,19 @@ package ejercicios;
  * @author vero
  */
 public class ejercicio2 {
-    
+    public static void main(String[] args){
+    String Usuario = "vero";
+    String clave = "hola";
+    if(Usuario == "vero"){
+        if(clave == "hola"){
+            System.out.println("Bienvenidos");
+        }
+        else{
+            System.out.println("clave incorrecta");
+        }
+    }
+    else{
+        System.out.println("No corresponde");
+        }
+    }
 }
